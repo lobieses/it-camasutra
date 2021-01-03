7feaee4969d1f4ef0749ca818bc033b256a8ea98
lobieses <76759635+lobieses@users.noreply.github.com>
2021-01-03 07:37:46 +0200

    №14 added css modules

--- src/components/content.jsx
+++ src/components/content.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import style from './content.module.css';
 
 class Content extends React.Component {
     render() {
         return (
-        <div className="content">
+        <div className={style.content}>
             <img alt="contentImg" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTU1dFR0DR4d0wILeQRaH1oo1Dlsag-Gp2dXg&usqp=CAU"/>
         </div>
         )
--- src/components/side-bar.jsx
+++ src/components/side-bar.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
+import style from './side-bar.module.css';
 
 class SideBar extends React.Component {
     render() {
         return (
-        <div className="side-bar">
-            <div className="elems">
-                <ul className="sideBarElems">
+        <div className={style.sideBar}>
+            <div className={style.sideBarBlock}>
+                <ul className={style.sideBarElems}>
                     <li>Profile</li>
                     <li>Message</li>
                     <li>News</li>
                     <li>Music</li>
                 </ul>
             </div>
-            <div className="elems">
-                <ul className="sideBarElems">
+            <div className={style.sideBarBlock}>
+                <ul className={style.sideBarElems}>
                     <li>Settings</li>
                 </ul>
             </div>
-            <div className="friends">
+            <div className={style.friends}>
                 
             </div>
         </div>
