d8622c2b69ee422ef1fbc2ba708b1bd5001de6f5
Sergii Vysoven <sergii.v@develops.today>
2022-03-18 22:12:07 +0200

    feat(api): create users request to new API

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,32 +1,35 @@
-import axios from 'axios';
-import {userType} from "../types/types";
+import axios from "axios";
+import { userType } from "../types/types";
 
 export const instance = axios.create({
-    withCredentials: true,
-    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
-    headers: {
-        'API-KEY': '718bf453-524a-48b2-8e84-c21eb40e723c'
-    }
+  withCredentials: true,
+  baseURL: "https://social-network.samuraijs.com/api/1.0/",
+  headers: {
+    "API-KEY": "718bf453-524a-48b2-8e84-c21eb40e723c",
+  },
+});
+
+export const LocalApiInstance = axios.create({
+  baseURL: "http://localhost:8000/",
 });
 
 export enum ResultCodes {
-    Success = 0,
-    Error = 1
+  Success = 0,
+  Error = 1,
 }
 
 export enum ResultCodeForCapctha {
-    CaptchaIsRequired = 10
+  CaptchaIsRequired = 10,
 }
 
 export type GetItemsType = {
-    items: Array<userType>,
-    totalCount: number,
-    error: null | string
-}
+  items: Array<userType>;
+  totalCount: number;
+  error: null | string;
+};
 
 export type APIResponseType<D = {}, RC = ResultCodes> = {
-    data: D,
-    messages: Array<string>,
-    resultCode: RC
-}
-
+  data: D;
+  messages: Array<string>;
+  resultCode: RC;
+};
--- src/api/users-api.ts
+++ src/api/users-api.ts
@@ -1,22 +1,26 @@
-import {APIResponseType, GetItemsType, instance} from "./api";
+import {
+  APIResponseType,
+  GetItemsType,
+  instance,
+  LocalApiInstance,
+} from "./api";
 
 export const usersAPI = {
-    getUsers(size = 3, page = 1) {
-        return instance.get<GetItemsType>(`users?count=${size}&page=${page}`)
-            .then((response) => {
-                return response.data;
-            });
-    },
-    follow(id: number) {
-        return instance.post<APIResponseType>(`follow/${id}`)
-            .then((response) => {
-                return response.data;
-            });
-    },
-    unFollow(id: number) {
-        return instance.delete(`follow/${id}`)
-            .then((response) => {
-                return response.data;
-            }) as Promise<APIResponseType>;
-    },
+  getUsers(size = 3, page = 1) {
+    return LocalApiInstance.get<GetItemsType>(
+      `getUsers?count=${size}&page=${page}`
+    ).then((response) => {
+      return response.data;
+    });
+  },
+  follow(id: number) {
+    return instance.post<APIResponseType>(`follow/${id}`).then((response) => {
+      return response.data;
+    });
+  },
+  unFollow(id: number) {
+    return instance.delete(`follow/${id}`).then((response) => {
+      return response.data;
+    }) as Promise<APIResponseType>;
+  },
 };
